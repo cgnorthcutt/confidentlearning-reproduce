e002cc789135bc9e0f73f19fdb54c211fb1d5cad
Curtis Northcutt <curtis.northcutt@gmail.com>
2020-02-20 05:19:48 -0500

    Add docstring comment support

--- cifar10/confidentlearning_and_coteaching/loss.py
+++ cifar10/confidentlearning_and_coteaching/loss.py
@@ -5,7 +5,45 @@ from torch.autograd import Variable
 import numpy as np
 
 # Loss functions
-def loss_coteaching(y_1, y_2, t, forget_rate, ind, noise_or_not, class_weights=None):
+def loss_coteaching(
+        y_1,
+        y_2,
+        t,
+        forget_rate,
+        class_weights=None,
+):
+    """Co-Teaching Loss function.
+    Source: https://github.com/bhanML/Co-teaching/blob/master/loss.py
+
+    Parameters
+    ----------
+    y_1 : Tensor array
+      Output logits from model 1
+
+    y_2 : Tensor array
+      Output logits from model 2
+
+    t : np.array
+      List of Noisy Labels (t standards for targets)
+
+    forget_rate : float
+      Decimal between 0 and 1 for how quickly the models forget what they learn.
+      Just use rate_schedule[epoch] for this value
+
+    ind : iterable / list / np.array
+      Indices from train_loader created like this:
+      for i, (images, labels, indexes) in enumerate(train_loader):
+        ind=indexes.cpu().numpy().transpose()
+
+    noise_or_not : np.array
+      Np.array of length number of total training examples in dataset that says
+      for every training example if its prediction is equal to its given label.
+      For each example in the training dataset, true if equal, else false.
+
+    class_weights : Tensor array, shape (Number of classes x 1), Default: None
+      A np.torch.tensor list of length number of classes with weights
+    """
+
     loss_1 = F.cross_entropy(y_1, t, reduce = False, weight=class_weights)
     ind_1_sorted = np.argsort(loss_1.data.cpu())
     loss_1_sorted = loss_1[ind_1_sorted]
